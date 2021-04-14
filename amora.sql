1896003e2fa72a2cccab39facf2682154554f34a
arcodev <chris_arco0794@hotmail.com>
2021-04-14 11:50:07 -0500

    Ajustes finales

--- amora.sql
+++ amora.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-04-2021 a las 20:23:56
+-- Tiempo de generación: 14-04-2021 a las 18:49:40
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.10
 
@@ -51,8 +51,8 @@ INSERT INTO `categorias` (`id_cat`, `nombre`, `editado`) VALUES
 
 CREATE TABLE `productos` (
   `id_pro` int(11) NOT NULL,
-  `nombre_precio` varchar(100) NOT NULL,
-  `id_cat` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `precio` varchar(60) NOT NULL,
   `url_foto` varchar(50) NOT NULL,
   `nombre_cat` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -61,20 +61,60 @@ CREATE TABLE `productos` (
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`id_pro`, `nombre_precio`, `id_cat`, `url_foto`, `nombre_cat`) VALUES
-(5, 'Anillo 1, $150', 1, 'anillo1.png', 'anillos'),
-(6, 'Anillo 2, $250', 1, 'anillo2.png', 'anillos'),
-(7, 'Anillo 2, $350', 1, 'anillo3.png', 'anillos'),
-(8, 'Aretes 1, $150', 2, 'aretes1.png', 'aretes'),
-(9, 'Aretes 2, $350', 2, 'aretes2.png', 'aretes'),
-(10, 'Aretes 3, $350', 2, 'aretes3.jpg', 'aretes'),
-(11, 'Collar 1, $150', 3, 'collar1.jpg', 'collares'),
-(12, 'Collar 2, $250', 3, 'collar2.png', 'collares'),
-(13, 'Collar 2, $350', 3, 'collar3.png', 'collares'),
-(14, 'Pulsera 1, $150', 4, 'pulseras1.png', 'collares'),
-(15, 'Pulsera 2, $350', 4, 'pulseras2.png', 'pulseras'),
-(16, 'Prueba, $122', 4, 'pulseras8.png', 'pulseras'),
-(18, 'Pulsera 4, $450', 0, 'pulseras4.png', 'pulseras');
+INSERT INTO `productos` (`id_pro`, `nombre`, `precio`, `url_foto`, `nombre_cat`) VALUES
+(1, 'Juego de collar de plata y dijes', '$260', 'FOTO2.jpg', 'collares'),
+(2, 'Juego de collar de plata', '$740', 'juego8.png', 'collares'),
+(3, 'Collar de plata floreal', '$690', 'juego7.png', 'collares'),
+(4, 'Collar de plata', '$690', 'juego18.png', 'collares'),
+(5, 'Juego collar de plata', '$1200', 'juego10.png', 'collares'),
+(6, 'Collar de plata', '$870', 'juego1.png', 'collares'),
+(7, 'Aretes de plata', '$300', 'aretes1.png', 'aretes'),
+(8, 'Collar de plata', '$500', 'FOTO2.png', 'collares'),
+(9, 'Collar corazÃ³n de plata', '$300', 'FOTO16_1.png', 'collares'),
+(10, 'Collar corazÃ³n de plata', '$300', 'FOTO6_3.png', 'collares'),
+(11, 'Collar de plata', '$280', 'FOTO9.png', 'collares'),
+(12, 'Collar de plata', '$270', 'FOTO2S.png', 'collares'),
+(13, 'Collar de plata', '$500', 'FOTO20.png', 'collares'),
+(14, 'Collar de plata', '$500', 'collar24.png', 'collares'),
+(15, 'Collar de plata', '$280', 'FOTO5_2.png', 'collares'),
+(16, 'Collar de plata', '$300', 'collar17.png', 'collares'),
+(17, 'Pulsera de plata', '$320', 'FOTO14.png', 'pulseras'),
+(18, 'Pulsera de plata', '$340', 'FOTO34.png', 'pulseras'),
+(19, 'Pulsera de plata', '$380', 'pulseras3.png', 'pulseras'),
+(20, 'Pulsera de plata', '$850', 'FOTO37.png', 'pulseras'),
+(21, 'Pulsera de plata', '$260', 'juego4.png', 'pulseras'),
+(22, 'Pulsera de plata', '$300', 'FOTO12.png', 'pulseras'),
+(23, 'Anillo de plata', '$220', 'FOTO3_1.png', 'anillos'),
+(24, 'Anillo de plata', '$220', 'FOTO2_4.png', 'anillos'),
+(25, 'Anillo de plata triple', '$450', 'juego3.png', 'anillos'),
+(26, 'Anillo de plata, baÃ±o de oro', '$540', 'FOTO19..png', 'anillos'),
+(27, 'Anillo de plata', '$480', 'FOTO3_5.png', 'anillos'),
+(28, 'Anillo de plata', '$450', 'FOTO7_1.png', 'anillos'),
+(29, 'Anillo de plata', '$295', 'anillo9.png', 'anillos'),
+(30, 'Anillo de plata triple, baÃ±o de oro', '$450', 'FOTO32.png', 'anillos'),
+(31, 'Anillo de plata', '$340', 'FOTO40.png', 'anillos'),
+(32, 'Aretes de plata', '$295', 'aretes8.png', 'aretes'),
+(33, 'Aretes de plata', '$270', 'FOTO12_2.png', 'aretes'),
+(34, 'Aretes de plata', '$295', 'FOTO4_4.png', 'aretes'),
+(35, 'Aretes de plata', '$135', 'FOTO15_2.png', 'aretes'),
+(36, 'Aretes de plata', '$390', 'aretes12.png', 'aretes'),
+(37, 'Aretes de plata', '$290', 'aretes13.png', 'aretes'),
+(38, 'Aretes de plata', '$295', 'aretes9.png', 'aretes'),
+(39, 'Aretes de plata', '$320', 'arete-1.png', 'aretes'),
+(40, 'Aretes de plata', '$200', 'juego13.png', 'aretes'),
+(41, 'Aretes de plata', '$140', 'juego14.png', 'aretes'),
+(42, 'Aretes de plata', '$265', 'juego12.png', 'aretes'),
+(43, 'Pulsera de plata', '$240', 'FOTO6_1.png', 'pulseras'),
+(44, 'Pulsera de plata', '$240', 'FOTO3_3.png', 'pulseras'),
+(45, 'Pulsera de plata', '$240', 'FOTO4_2.png', 'pulseras'),
+(46, 'Pulsera de plata', '$240', 'FOTO1_4.png', 'pulseras'),
+(47, 'Pulsera de plata', '$280', 'FOTO5.png', 'pulseras'),
+(48, 'Pulsera de plata', '$220', 'FOTO3_4.png', 'pulseras'),
+(49, 'Pulsera de plata', '$160', 'pulseras7.png', 'pulseras'),
+(50, 'Pulsera de plata', '$290', 'FOTO18.png', 'pulseras'),
+(51, 'Pulsera de plata', '$300', 'FOTO27.png', 'pulseras'),
+(52, 'Anillo pico, de plata', '$300', 'FOTO24.png', 'anillos'),
+(53, 'Collar chapa de oro', '$550', 'collar25.png', 'collares');
 
 -- --------------------------------------------------------
 
@@ -136,7 +176,7 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id_pro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_pro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
